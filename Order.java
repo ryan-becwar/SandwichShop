6557f8c12872d0f81b0b97ca4e99da30a918a937
Mary Glenn <mgglenn@g.clemson.edu>
2015-03-09 14:06:30 -0400

    Started Order.java

--- Order.java
+++ Order.java
@@ -3,8 +3,18 @@
 */
 
 public class Order {
-
-
-
+	
+	private String customerName; //name of customer, eg Customer1 or John Smith
+	private int[] ticket; //contains amount of each item ordered
+	
+	public Order() {
+		
+		customerName = "Customer #1";
+		//ticket already intitalized!
+	}
+	
+	public String getName() {return customerName;}
+	
+	public int[] getTicket() {return ticket;}
 
 }
