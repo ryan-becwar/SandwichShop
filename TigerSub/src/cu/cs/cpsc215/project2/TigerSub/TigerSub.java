a87c00bb37b5c788e85e87e652d1c258cae67fdd
Mary Glenn <mgglenn@g.clemson.edu>
2015-03-21 13:56:37 -0400

    tigersub changes

--- TigerSub/src/cu/cs/cpsc215/project2/TigerSub/TigerSub.java
+++ TigerSub/src/cu/cs/cpsc215/project2/TigerSub/TigerSub.java
@@ -2,8 +2,20 @@
 package cu.cs.cpsc215.project2.TigerSub;
 
 public class TigerSub {
+	/**
+	private int numCustomers = 0;
+	private boolean simulate = false;
+	private int randomSeed = 0;
+	
+	public TigerSub(String[] args) {
+		if(args.length >= 1) {
+			if(args[0].)
+		}
+	}
+	*/
 
     public static void main(String[] args){
+    	
     	
     	//Determine which parameters the user gave
     	
